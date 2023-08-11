dc8fb64b68f8e89a593a55615c7ada9598842654
Sarah Schröder <saschroeder@techfak.uni-bielefeld.de>
2023-08-11 15:01:42 +0200

    relax version requirements

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(name='agml-bias-score',
       version=version,
       description='Implementation and wrapper of bias scores for NLP',
       url='https://github.com/UBI-AGML-NLP/EmbeddingBiasScores',
-      packages=['geometrical_bias', 'lipstick_bias'],
+      packages=['geometrical_bias', 'lipstick_bias', 'unmasking_bias'],
       install_requires=INSTALL_REQUIRES,
       )
