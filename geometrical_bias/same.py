5e72d3612e68d42df45ba6b39ebf63bd3a3fde99
Sarah Schröder <saschroeder@techfak.uni-bielefeld.de>
2024-04-22 09:51:50 +0200

    renaming variables for same

--- geometrical_bias/same.py
+++ geometrical_bias/same.py
@@ -10,7 +10,7 @@ class SAME(GeometricBias):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.pairs = None
+        self.bias_components = None
 
     def attr_mean(self, attribute_set):
         A_unit = normalize(attribute_set)
@@ -27,73 +27,73 @@ class SAME(GeometricBias):
         self.A = attribute_sets
 
         # compute pairwise bias directions in advance
-        self.pairs = [self.attr_pair_diff(attribute_sets[1], attribute_sets[0])]
+        self.bias_components = [self.attr_pair_diff(attribute_sets[1], attribute_sets[0])]
 
         for i in range(2, self.n):
             # remove correlation with previous bias directions (otherwise we might observe the same bias twice)
             # we only need bias directions of pairs (attribute_sets[i], attribute_sets[0]), because others can be
             # written as a linear combination of these
-            new_pair = self.attr_pair_diff(attribute_sets[i], attribute_sets[0])
-            for pair in self.pairs:
-                pair = pair / np.linalg.norm(pair)
-                corr = np.dot(new_pair, pair)
-                v_corr = corr * pair
-                new_pair = new_pair - v_corr
-            self.pairs.append(new_pair)
+            new_component = self.attr_pair_diff(attribute_sets[i], attribute_sets[0])
+            for comp in self.bias_components:
+                comp = comp / np.linalg.norm(comp)
+                corr = np.dot(new_component, comp)
+                v_corr = corr * comp
+                new_component = new_component - v_corr
+            self.bias_components.append(new_component)
 
-    def pair_bias(self, target, pair_diff):
-        return cossim(target, pair_diff)
+    def component_bias(self, target, component):
+        return cossim(target, component)
 
     def signed_individual_bias(self, target: np.ndarray):
         assert self.n == 2, "signed bias can only be obtained for exactly 2 groups"
 
         w = target / np.linalg.norm(target)  # need unit vectors!
-        return self.pair_bias(w, self.pairs[0])
+        return self.component_bias(w, self.bias_components[0])
 
     def individual_bias(self, target: np.ndarray):
         assert self.n >= 2
 
         w = target / np.linalg.norm(target)  # need unit vectors!
 
-        # initialize bias with bias to first pair
-        bias = abs(self.pair_bias(w, self.pairs[0]))
-        if len(self.pairs) == 1:
+        # initialize bias with bias to first component
+        bias = abs(self.component_bias(w, self.bias_components[0]))
+        if len(self.bias_components) == 1:
             return bias
 
-        for i in range(1, len(self.pairs)):
-            bias += abs(self.pair_bias(w, self.pairs[i])*np.linalg.norm(self.pairs[i]))
+        for i in range(1, len(self.bias_components)):
+            bias += abs(self.component_bias(w, self.bias_components[i]) * np.linalg.norm(self.bias_components[i]))
 
         return bias
 
     def mean_individual_bias(self, targets: EmbSet):
         return np.mean([self.individual_bias(target) for target in targets])
     
-    def individual_bias_per_pair(self, target: np.ndarray):
+    def individual_bias_per_component(self, target: np.ndarray):
         assert self.n >= 2
 
         w = target / np.linalg.norm(target)  # need unit vectors!
         
         biases = []
-        biases.append(self.pair_bias(w, self.pairs[0]))
+        biases.append(self.component_bias(w, self.bias_components[0]))
         
-        for i in range(1, len(self.pairs)):
-            biases.append(self.pair_bias(w, self.pairs[i]))
+        for i in range(1, len(self.bias_components)):
+            biases.append(self.component_bias(w, self.bias_components[i]))
         return biases
 
     def individual_bias_pairwise(self, target: np.ndarray, group1: int, group2: int):
         assert (0 <= group1 < self.n and 0 <= group2 < self.n and not group1 == group2)
         pair_diff = self.attr_pair_diff(self.A[group1], self.A[group2])
-        return self.pair_bias(target, pair_diff)
+        return self.component_bias(target, pair_diff)
 
     def skew_pairwise(self, targets: EmbSet, group1: int, group2: int):
         assert (0 <= group1 < self.n and 0 <= group2 < self.n and not group1 == group2)
         pair_diff = self.attr_pair_diff(self.A[group1], self.A[group2])
-        return np.mean([self.pair_bias(w, pair_diff) for w in targets])
+        return np.mean([self.component_bias(w, pair_diff) for w in targets])
 
     def stereotype_pairwise(self, targets: EmbSet, group1: int, group2: int):
         assert (0 <= group1 < self.n and 0 <= group2 < self.n and not group1 == group2)
         pair_diff = self.attr_pair_diff(self.A[group1], self.A[group2])
-        return np.std([self.pair_bias(w, pair_diff) for w in targets])
+        return np.std([self.component_bias(w, pair_diff) for w in targets])
 
     def group_bias(self, target_groups: EmbSetList):
         print("group bias is not implemented for SAME")
